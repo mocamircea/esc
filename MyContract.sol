c04098c6c42edfaba2c7966fc7e9f18cf97413a0
mocamircea <mocamircea@gmail.com>
2019-02-11 15:25:58 +0000

    added onlyCompute function

--- MyContract.sol
+++ MyContract.sol
@@ -1,10 +1,14 @@
-pragma solidity 0.4.22;
+pragma solidity 0.4.18;
 
 contract MyContract{
     address owner;
     
     function constructor() public {
         owner = msg.sender;
+    }
+    
+    function onlyCompute() external pure returns(uint8 result){
+        return 3+3;
     }
 }
 
