cd753251f988390604cfe983ce5d1d21df97b9ac
mocamircea <mocamircea@gmail.com>
2019-11-01 13:31:50 +0000

    more events

--- Events.sol
+++ Events.sol
@@ -3,24 +3,33 @@ pragma solidity 0.4.18;
 contract Events{
     address owner;
     
-    event LogComputeVal(address sender, uint8 val1, uint8 val2);
+    event LogSum(indexed address sender, uint8 val1, uint8 val2);
+    event LogSubtract(indexed address sender, uint8 val1, uint8 val2);
     
     function constructor() public {
         owner = msg.sender;
     }
     
     // 1956 gas
-    function compute1(uint8 lval, uint8 rval) external returns(uint8 result){
+    function sum1(uint8 lval, uint8 rval) external returns(uint8 result){
         uint8 resultsum = lval+rval;
-        LogComputeVal(msg.sender, lval, rval);
+        LogSum(msg.sender, lval, rval);
         
         return resultsum;
     }
     
     // 1965 gas
-    function compute2(uint8 lval, uint8 rval) external returns(uint8 result){
-        LogComputeVal(msg.sender, lval, rval);
+    function sum2(uint8 lval, uint8 rval) external returns(uint8 result){
+        LogSum(msg.sender, lval, rval);
         
         return lval+rval;  // more expensive to calculate directly in the returned value <-> to using additional stack variable
     }
+    
+    function sub(uint8 lval, uint8 rval) external returns(uint8 result){
+        uint8 resultsub = lval-rval;
+        LogSubtract(msg.sender, lval, rval);
+        
+        return resultsub;
+    }
+    
 }
